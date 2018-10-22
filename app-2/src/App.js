5d2f6c793fd8980c1f801598caa1ff1f332fcb2c
Brian Jones <BriansMac@Brians-MacBook-Pro.local>
2018-10-22 15:01:25 -0600

    finished

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,7 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      array : [
+        "Brian",
+        "Douglas",
+        "Jones"
+      ]
+    }
+  }
+  
   render() {
+    let array = this.state.array
     return (
       <div className="App">
         <div className="App-header">
@@ -11,7 +24,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <ul>
+          {array.map(array => <h2>{array}</h2>)}
+          </ul>
         </p>
       </div>
     );
